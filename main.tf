68218c38427af3ee47873a9c79951f72ee549c84
krishan <krbahin@email.com>
2025-08-03 16:38:04 +0000

    create a s3 with terrafor test

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
- this is the first push
+# main.tf
+provider "google" {
+  project = "it-dev-461717"
+  region  = "us-central1" # optional for bucket
+}
+
+resource "google_storage_bucket" "terra_test" {
+  name          = "terra-test"
+  location      = "US"
+  storage_class = "STANDARD"
+  force_destroy = true # allows bucket deletion even if it contains objects
+}
+
